249dbf3dd1b682c68fd7f2cb812f1ba4ef4b8ffd
hamza <hamza_pisces@live.com>
2019-07-11 15:16:19 +0500

    project phase 1 final design

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -5,8 +5,20 @@
 <div class="row panel">
      <div class="col-md-9">
     <p class="user-project_content">My Projects / {{$project->title}}<button class="float-right user-project_content_btn">Add Task</button></p>
-   <div class="tasks">
-       <span>task</span>
+   <div class="task">
+    <p class="user-project_content">Tasks</p>
+     <div class="task-card">
+        run berry run
+    </div>
+    <div class="task-card">
+        <input type="text" name="task" placeholder="Add Task" class="task-card_input">
+    </div>
+   </div>
+   <div class="notes">
+       <p class="user-project_content">General Notes</p>
+       <div class="form-group">
+           <textarea name="notes" id="" cols="30" rows="10" class="form-control"></textarea>
+       </div>
    </div>  
     </div>
     <div class="col-md-3 single-project">
